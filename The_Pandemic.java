a12913034f67caf4aae1a39781266f0f8e1f82f3
charan233 <106661798+charan233@users.noreply.github.com>
2022-09-20 22:31:34 +0530

    import java.util.Scanner; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int h;         h=sc.nextInt();         if(h<3)         {             System.out.println(h-1);         }         else         {             System.out.println(2);         }
        }
    }

---
+++ The_Pandemic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int h;
+        h=sc.nextInt();
+        if(h<3)
+        {
+            System.out.println(h-1);
+        }
+        else
+        {
+            System.out.println(2);
+        }
+        
+        
+         
+    }
+}
