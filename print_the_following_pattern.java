060de52e882720ab3993ff7a0f349824c1b20662
charan233 <106661798+charan233@users.noreply.github.com>
2022-10-14 20:59:34 +0530

    import java.util.Scanner; class sample {
    	public static void main(String[] args)
    	{
    		
    		Scanner sc=new Scanner(System.in);
    		int r;
    		r=sc.nextInt();
    		for(int i=1;i<=2*r/2;i++)			 
    		{   
    		    
    			int k=i>r?r-i%r:i;
    			for(int j=1;j<=r;j++)
    			{
    			    if(j<=r-k)
    				
    					System.out.print(" ");
    					
    					
    				
    				
    			}
    			for(int p=1;p<=2*k-1;p++)
    			System.out.print(i);
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -6,21 +6,24 @@ class sample
 	{
 		
 		Scanner sc=new Scanner(System.in);
-		int n;
-		n=sc.nextInt();
-		for(int i=1;i<=n;i++)			 
+		int r;
+		r=sc.nextInt();
+		for(int i=1;i<=2*r/2;i++)			 
 		{   
 		    
-			for(int j=n;j<=n&&j>0;j--)	 
+			int k=i>r?r-i%r:i;
+			for(int j=1;j<=r;j++)
 			{
-			    
+			    if(j<=r-k)
 				
-					System.out.print(j+" ");
+					System.out.print(" ");
 					
 					
 				
 				
 			}
+			for(int p=1;p<=2*k-1;p++)
+			System.out.print(i);
 			System.out.println();
 		}
 	}
